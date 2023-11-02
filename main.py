add778068deb5e380a6bc40fa3dc48028aba4cee
UreshiiSushi <merieru@gmail.com>
2023-11-02 21:43:05 +0100

    start HW 9

---
+++ main.py
@@ -0,0 +1,16 @@
+from pprint import pprint
+from collections import defaultdict
+
+phone_book = {}
+
+def main():
+    while True:
+        user_input = input(">>> ")
+        command = user_input.split(" ")
+        if ...:
+            ...
+
+
+if __name__ == "__main__":
+    main()
+    
